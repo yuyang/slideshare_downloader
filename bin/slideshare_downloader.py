f12d757e9424b411dce809c88cc743bc680fa731
yuyang <blesslyy@gmail.com>
2015-04-17 05:12:37 +0000

    make it work

--- bin/slideshare_downloader.py
+++ bin/slideshare_downloader.py
@@ -13,7 +13,6 @@
 # http://www.slideshare.net/thegaragegroup/new-approaches-to-business-model-innovation \
 # -f abc.pdf
 
-import Image
 import pycurl
 import StringIO
 import os
@@ -24,7 +23,6 @@ import re
 
 parser = argparse.ArgumentParser(description='该工具用于下载slideshark上的文档并转为pdf格式.')   # This and preceding 4 lines used to control the arguments entered in the CLI.
 parser.add_argument('-f', action="store", dest='file_name', help='pdf文件的名称')
-parser.add_argument('-p', action="store", dest='proxy', help='设置http代理')
 parser.add_argument('-s', action="store", dest='url', help='文档的URL')
 
 args = parser.parse_args()
@@ -36,26 +34,9 @@ if len(sys.argv) == 1:  # 当用户没有输入任何参数时显示帮助并退
 args = vars(args)   # converts the arguments into dictionary format for easier retrieval.
 
 
-def get_result(url):
-    buf = StringIO.StringIO()
-    c = pycurl.Curl()
-    c.setopt(c.URL, url)
-    c.setopt(c.WRITEFUNCTION, buf.write)
-    c.setopt(c.CONNECTTIMEOUT, 5)
-    c.setopt(c.TIMEOUT, 8)
-    if args['proxy'] is not None:
-        c.setopt(c.PROXY, args['proxy'])
-    c.perform()
-    html = buf.getvalue()
-    buf.close()
-    return html
-
-
 def get_img_url(html):
-    img_url = re.findall('og:image.*?http://image.slidesharecdn.com/.*?slide-1-1024.jpg', html)
-    img_url = re.findall('http://image.slidesharecdn.com/.*?slide', img_url[0])
-    return img_url[0]
-
+    m = re.search(r'full="(http://image\.slidesharecdn\.com/.*?)-1-1024\.jpg\?', html)
+    return m.group(1)
 
 def get_pages(html):
     pages = re.findall('"slide_count":\d*', html)
@@ -63,9 +44,21 @@ def get_pages(html):
     pages = int(pages[0])
     return pages
 
+def get_result(url):
+    buf = StringIO.StringIO()
+    c = pycurl.Curl()
+    c.setopt(c.URL, url)
+    c.setopt(c.WRITEFUNCTION, buf.write)
+    c.setopt(c.CONNECTTIMEOUT, 5)
+    c.setopt(c.TIMEOUT, 8)
+    c.perform()
+    html = buf.getvalue()
+    buf.close()
+    return html
+
 
 def download_image(url, number):
-    print "downloading image please wait"
+    print "downloading %s image please wait"  % number
     toolbar_width = number
     # setup toolbar
     sys.stdout.write("[%s]" % (" " * toolbar_width))
@@ -74,11 +67,9 @@ def download_image(url, number):
     number = number + 1
     for i in range(1, number):
         img_url = '%s-%s-1024.jpg' % (url, i)
-        img = get_result(img_url)
-        img = Image.open(StringIO.StringIO(img))
-        img.save('../output/%s.jpg' % i)
-        sys.stdout.write("-")
-        sys.stdout.flush()
+        cmd = 'wget -x -q -O ../output/%03d.jpg %s' % (i, img_url)
+        print cmd
+        os.system(cmd)
     sys.stdout.write("\n")
 
 
@@ -87,19 +78,10 @@ def convert_to_pdf(filename):
     print "%s has been created at folder output please check" % filename
     filelist = [f for f in os.listdir("../output") if f.endswith(".jpg")]
     print "removing images"
-    toolbar_width = len(filelist)
-    # setup toolbar
-    sys.stdout.write("[%s]" % (" " * toolbar_width))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (toolbar_width + 1))  # return to start of line, after '['
-    for f in filelist:
-        os.remove("../output/%s" % f)
-        sys.stdout.write("-")
-        sys.stdout.flush()
-    sys.stdout.write("\n")
-
+    os.system('rm ../output/*.jpg')
 
 if __name__ == '__main__':
+    os.system('rm -f ../output/*')
     html = get_result(args['url'])
     url = get_img_url(html)
     pages = get_pages(html)
